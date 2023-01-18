9a071addcb4ab374ccef7e3b0f8a32840098e3c6
recursive-beast <splentercell.1997@gmail.com>
2023-01-18 12:02:38 +0100

    use dynamice server instance instead of static

--- main.cpp
+++ main.cpp
@@ -3,40 +3,39 @@
 #include <poll.h>
 #include <iostream>
 
-void	handleLine(std::string line, Server *server, Client *client)
-{
+void	handleLine(std::string line, Server *server, Client *client) {
 	(void)server;
 	client->write(line + "\n");
 	std::cout << "[" << client->addr << ":" << client->port << "]#" << client->sd << ": line: " << line << std::endl;
 }
 
-void	handleConnect(Server *server, Client *client)
-{
+void	handleConnect(Server *server, Client *client) {
 	(void)server;
 	std::cout << "[" << client->addr << ":" << client->port << "]#" << client->sd << ": connected" << std::endl;
 }
 
-void	handleDisconnect(Server *server, Client *client)
-{
+void	handleDisconnect(Server *server, Client *client) {
 	(void)server;
 	std::cout << "[" << client->addr << ":" << client->port << "]#" << client->sd << ": disconnected" << std::endl;
 }
 
 int main (int argc, char **argv) {
+	Server	*server = NULL;
+
 	try {
 		if (argc != 3)
 			throw std::runtime_error("Invalid arg count");
-
-		Server	server(std::stoi(argv[1]), std::string(argv[2]));
-
-		server.onLine = handleLine;
-		server.onConnect = handleConnect;
-		server.onDisconnect = handleDisconnect;
-		server.poll();
+		server = new Server(std::stoi(argv[1]), std::string(argv[2]));
+		server->onLine = handleLine;
+		server->onConnect = handleConnect;
+		server->onDisconnect = handleDisconnect;
+		server->poll();
+		delete server;
 		return (0);
 	}
 	catch(const std::exception& e) {
 		std::cerr << e.what() << std::endl;
+		delete server;
 		return (1);
 	}
 }
