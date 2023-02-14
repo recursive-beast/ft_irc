d04b5c2b94100c737ecda88e29b973358b2a23ef
recursive-beast <splentercell.1997@gmail.com>
2023-02-14 07:00:00 +0100

    do not register a client unless correct password and nickname is given

--- commands/USER.cpp
+++ commands/USER.cpp
@@ -5,12 +5,14 @@
 std::string	USER(Message msg, Server *server, Client *client) {
 	if (client->registered)
 		return (ERR_ALREADYREGISTRED(client));
+	if (server->password != client->password)
+		return (ERR_PASSWDMISMATCH(client));
+	if (!client->getNickname().length())
+		return (ERR_NONICKNAMEGIVEN(client));
 	if (msg.params.size() < 4 || msg.params[3].length() == 0)
 		return (ERR_NEEDMOREPARAMS(client, msg.cmd));
 	if (!isuser(msg.params[0]))
 		return (ERR_ERRONEUSUSERNAME(client, msg.cmd));
-	if (server->password != client->password)
-		return (ERR_PASSWDMISMATCH(client));
 	client->username = msg.params[0];
 	client->realname = msg.params[3];
 	client->registered = true;
